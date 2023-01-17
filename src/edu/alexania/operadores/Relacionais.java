cd12243531164055b8f8518822f7bdc7fde78145
alexaniatoma <alexaniatoma@hotmail.com>
2023-01-17 15:55:55 -0300

    entendendo os operadores relacionais

---
+++ src/edu/alexania/operadores/Relacionais.java
@@ -0,0 +1,13 @@
+package edu.alexania.operadores;
+
+public class Relacionais {
+    
+    public static void main(String[] args) {
+        String nomeUm = "Alexania";
+        String nomeDois = new String ("Alexania");
+
+        System.out.println(nomeUm.equals(nomeDois));
+    }
+}
+
+//objetos utilizar o equals
