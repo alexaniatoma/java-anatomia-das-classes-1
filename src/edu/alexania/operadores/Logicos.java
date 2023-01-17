c757266b6ef3a73d3dd42e405833846ac06f2569
alexaniatoma <alexaniatoma@hotmail.com>
2023-01-17 16:03:43 -0300

    operadores condicionais

---
+++ src/edu/alexania/operadores/Logicos.java
@@ -0,0 +1,25 @@
+package edu.alexania.operadores;
+
+public class Logicos {
+    public static void main(String[] args) {
+
+        boolean condicao1 = true;
+
+        boolean condicao2 = false;
+
+        if(condicao1 && (7 > 4)) {
+            System.out.println("As duas condições são verdadeiras");
+        }
+
+        if(condicao1 || condicao2) {
+            System.out.println("As duas condições são verdadeiras");
+
+        }
+
+        System.out.println("fim");        
+    }
+    
+}
+
+//&& operador E 
+// || operador OU
