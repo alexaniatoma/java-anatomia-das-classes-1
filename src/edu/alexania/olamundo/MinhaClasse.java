02b49be35be71d5c3db4a456012b399573913610
alexaniatoma <alexaniatoma@hotmail.com>
2023-01-17 13:27:39 -0300

    criando projeto de tipos e variaveis

---
+++ src/edu/alexania/olamundo/MinhaClasse.java
@@ -0,0 +1,23 @@
+package edu.alexania.olamundo;
+public class MinhaClasse {
+     public static void main (String [] args) {
+        String primeiroNome = "Alexania";
+        String segundoNome = "Toma";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+        System.out.print(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+
+
+
+}
+
+
+    
+
