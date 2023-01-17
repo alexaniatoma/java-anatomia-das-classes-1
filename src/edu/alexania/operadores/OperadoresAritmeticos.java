19eccba73528e0e56dd5eb45b1bf5b7734a09f93
alexaniatoma <alexaniatoma@hotmail.com>
2023-01-17 15:17:40 -0300

    operadores aritmeticos

---
+++ src/edu/alexania/operadores/OperadoresAritmeticos.java
@@ -0,0 +1,26 @@
+package edu.alexania.operadores;
+
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        double soma = 10.5 + 15.7;
+        System.out.println(soma);
+        
+        int subtracao = 113 - 25;
+        System.out.println(subtracao);
+
+        int multiplicacao = 20 * 7;
+        System.out.println(multiplicacao);
+
+        int divisao = 15/3;
+        System.out.println(divisao);
+
+        int modulo = 18 % 3;
+        System.out.println(modulo);
+
+        double resultado = (10 * 7 ) + (20 / 4);
+        System.out.println(resultado);
+
+        
+    }
+    
+}
---
+++ src/edu/alexania/tiposvariaveis/TiposVariaveis.java
@@ -0,0 +1,22 @@
+package edu.alexania.tiposvariaveis;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        //tipos primitivos - int , byte, short, long, float, double, boolean, char
+
+        
+        //String usar ""
+        
+
+        //variaveis constantes usar a caixa alta e final
+
+
+        //double soma = 10.5 + 15.7;
+        //System.out.print(soma);
+        
+        int subtracao = 113 - 25;
+        System.out.print(subtracao);
+
+    }
+    
+}
