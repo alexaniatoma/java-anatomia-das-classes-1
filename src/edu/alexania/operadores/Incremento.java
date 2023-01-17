aec2c595953c7321cb0ce8299b295110c83ec715
alexaniatoma <alexaniatoma@hotmail.com>
2023-01-17 15:42:33 -0300

    operadores ternarios

---
+++ src/edu/alexania/operadores/Incremento.java
@@ -0,0 +1,12 @@
+package edu.alexania.operadores;
+
+public class Incremento {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        numero++;
+
+        System.out.println(numero);
+    }
+    
+}
